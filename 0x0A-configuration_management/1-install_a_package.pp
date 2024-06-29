16b78ab4e8f4d64906b39045ada3787774ca871c
ismael <spacingismael@gmail.com>
2024-06-29 02:27:57 +0100

    fixing task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 # install a package
 
+package { ['python3', python3-pip]:
+  ensure => installed,
+}
+
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
