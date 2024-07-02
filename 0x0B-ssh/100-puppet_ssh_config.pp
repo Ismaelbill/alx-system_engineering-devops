43c924c96540446e74b73becfc7c4e302ddcb15e
ismael <spacingismael@gmail.com>
2024-07-03 00:43:23 +0100

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,12 +2,12 @@
 
 file_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
-  line   => '  IdentityFile ~/.ssh/school',
+  line   => 'IdentityFile ~/.ssh/school',
   ensure => 'present',
 }
 
 file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',
-  line   => '  PasswordAuthentication no',
+  line   => 'PasswordAuthentication no',
   ensure => 'present',
 }
