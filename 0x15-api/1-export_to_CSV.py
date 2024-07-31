5982f2e69f7900f490197b1cec687ddeb683fa1f
ismael <spacingismael@gmail.com>
2024-07-31 14:43:19 +0100

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" extending Python script file 0 to export data in the CSV forma"""
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    urlUsers = "https://jsonplaceholder.typicode.com/users/{}"\
+        .format(sys.argv[1])
+    rUsers = requests.get(urlUsers)
+    urlTodos = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+        .format(sys.argv[1])
+    rTodos = requests.get(urlTodos)
+
+    with open("{}.csv".format(sys.argv[1]), 'a') as f:
+        for i in rTodos.json():
+            f.write('"{}","{}","{}","{}"\n'
+                    .format(sys.argv[1], rUsers.json()['username'],
+                            i['completed'], i['title']))
