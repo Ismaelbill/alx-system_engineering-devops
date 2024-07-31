198e011ba8e3f84253697b522fd57cc4b433bb2e
ismael <spacingismael@gmail.com>
2024-07-31 16:14:57 +0100

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    urlUsers = "https://jsonplaceholder.typicode.com/users/{}"\
+        .format(sys.argv[1])
+    rUsers = requests.get(urlUsers)
+    urlTodos = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+        .format(sys.argv[1])
+    rTodos = requests.get(urlTodos)
+    check = False
+    with open("{}.json".format(sys.argv[1]), 'a') as f:
+        f.write('{{"{}": ['.format(sys.argv[1]))
+        for i in rTodos.json():
+            if check:
+                f.write(", ")
+            check = True
+            f.write('{{"task": "{}", "completed": {}, "username": "{}"}}'
+                .format(i['title'], str(i['completed']).lower(), rUsers.json()['username']))
+        f.write(']}')
