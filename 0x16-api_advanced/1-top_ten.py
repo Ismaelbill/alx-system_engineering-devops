1e0226bf096ab200f4bd3d8c9b0935c8f838ec97
ismael <spacingismael@gmail.com>
2024-08-14 22:30:42 +0100

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" Top Ten
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ function that queries the Reddit API and prints
+    the titles of the first 10 hot posts listed for a given subreddit
+    """
+    base_url = "https://www.reddit.com"
+
+    headers = {'Accept': 'application/json',
+               'User-Agent': 'My User Agent 1.0'}
+    r = requests.get('{}/r/{}/.json?\
+                      sort=top&limit=10'.format(base_url, subreddit),
+                     headers=headers, allow_redirects=False)
+
+    if r.status_code == 200:
+        for post in r.json()['data']['children'][0:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
