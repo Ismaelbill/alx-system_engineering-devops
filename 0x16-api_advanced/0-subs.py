8f5da28e3a7abec99e46a9ef759b911f926938fb
ismael <spacingismael@gmail.com>
2024-08-14 01:08:42 +0100

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" How many subs? """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ function that queries the Reddit API and
+    returns the number of subscribers """
+
+    url = 'https://www.reddit.com'
+    r = requests.get('{}/r/{}/about/.json'.format(url, subreddit))
+    if r.status_code == 200:
+        return (r.json()['data']['subscribers'])
+    return 0
